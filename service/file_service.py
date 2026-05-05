74cd660a1e0ed52d365ac04ef039d80211c639b7
Iosif Vieru <iosifvieru440@gmail.com>
2026-05-05 20:28:56 +0300

    add maximum size check for file upload

--- service/file_service.py
+++ service/file_service.py
@@ -3,10 +3,24 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from client import s3_client
 from uuid import uuid4
 
+MAX_ALLOWED_UPLOAD_SIZE = 2_000_000_000 # bytes
+MAX_ALLOWED_UPLOAD_SIZE_GB = MAX_ALLOWED_UPLOAD_SIZE / 10e8
+
+def get_uploaded_files_size(uploaded_files: list[UploadFile]) -> int:
+    file_sizes = 0
+
+    for file in uploaded_files:
+        file_sizes += file.size
+
+    return file_sizes
+
 def upload_file_to_s3(files: list[UploadFile], bucket_name: str):
     if not files:
         raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="No files provided")
-    
+
+    if MAX_ALLOWED_UPLOAD_SIZE >= get_uploaded_files_size(files):
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail=f"Files are larger than {MAX_ALLOWED_UPLOAD_SIZE_GB} GB")
+
     uploaded = []
     failed = []
 
