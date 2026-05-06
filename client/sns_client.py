816dc944632ced2c54fab8606b494593c55cc14d
Iosif Vieru <iosifvieru440@gmail.com>
2026-05-06 16:24:58 +0300

    fix sns_client

--- client/sns_client.py
+++ client/sns_client.py
@@ -2,19 +2,19 @@ from os import getenv
 import boto3
 from loguru import logger
 
+SNS_ACCESS_KEY = getenv("SNS_ACCESS_KEY", "test")
+SNS_SECRET_KEY = getenv("SNS_SECRET_KEY", "test")
+
 SNS_AWS_REGION = getenv("SNS_AWS_REGION", "eu-central-1")
 SNS_AWS_ENDPOINT_URL = getenv("SNS_AWS_ENDPOINT_URL", "http://localhost:4566")
-SNS_TOPIC_ARN = getenv(
-    "SNS_TOPIC_ARN",
-    "arn:aws:sns:eu-central-1:000000000000:email-topic"
-)
+SNS_TOPIC_ARN = getenv("SNS_TOPIC_ARN", "arn:aws:sns:eu-central-1:000000000000:email-topic")
 
 sns = boto3.client(
     "sns",
     region_name=SNS_AWS_REGION,
     endpoint_url=SNS_AWS_ENDPOINT_URL,
-    aws_access_key_id="test",
-    aws_secret_access_key="test",
+    aws_access_key_id=SNS_ACCESS_KEY,
+    aws_secret_access_key=SNS_SECRET_KEY,
 )
 
 def publish_message(message: dict):
